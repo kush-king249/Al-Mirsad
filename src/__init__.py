db83a115f8cfa9730faca139d744a40ec007b691
Hassan shafie <hassanshafie19999@gmail.com>
2025-10-17 01:53:43 +0300

    Add files via upload

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Al-Mirsad - أداة أتمتة للاستجابة للحوادث الأمنية
+Incident Response Automation Tool
+
+Author: Hassan Mohamed Hassan Ahmed
+GitHub: kush-king249
+"""
+
+__version__ = "1.0.0"
+__author__ = "Hassan Mohamed Hassan Ahmed"
+__email__ = ""
+__description__ = "أداة أتمتة للاستجابة للحوادث الأمنية - Incident Response Automation Tool"
